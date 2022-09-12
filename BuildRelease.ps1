c731475af493ef781caf65e0821e2b2fe913ed98
dummycount <dummymal@hotmail.com>
2022-09-11 19:27:01 -0500

    Make zip in release script

--- BuildRelease.ps1
+++ BuildRelease.ps1
@@ -1,3 +1,4 @@
+# Build
 $vs = ConvertFrom-Json $(&'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -format json | Out-String)
 $msBuild = Join-Path $vs.installationPath 'Msbuild/Current/Bin/MSBuild.exe'
 
@@ -8,7 +9,8 @@ if (!$?) {
     exit
 }
 
-$out = "$PSScriptRoot/Release"
+# Copy to release folder
+$out = "$PSScriptRoot/Release/Pso2Cli"
 New-Item -ItemType Directory -Force -Path $out | Out-Null
 
 Remove-Item "$out/*"
@@ -17,3 +19,13 @@ Copy-Item -Path "$PSScriptRoot/Aqp2Fbx/bin/Release/*" -Destination $out -Recurse
 Copy-Item -Path "$PSScriptRoot/Dds2Png/bin/Release/*" -Destination $out -Recurse
 Copy-Item -Path "$PSScriptRoot/Fbx2Aqp/bin/Release/*" -Destination $out -Recurse
 Copy-Item -Path "$PSScriptRoot/IceCli/bin/Release/*" -Destination $out -Recurse
+
+# Remove empty directories
+foreach ($subdir in Get-ChildItem $out -Directory) {
+    if ($null -eq (Get-ChildItem $subdir)) {
+        Remove-Item $subdir
+    }
+}
+
+# Archive the release
+Compress-Archive -Path $out -DestinationPath "$PSScriptRoot/Release/Pso2Cli.zip"
